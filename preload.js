78409fec050ae088f67439724422a2a59a8c7672
yukino <3181137349go@gmail.com>
2023-06-14 22:42:36 +0800

    06-24

---
+++ preload.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector)
+        if (element) element.innerText = text
+    }
+
+    for (const dependency of ['chrome', 'node', 'electron']) {
+        replaceText(`${dependency}-version`, process.versions[dependency])
+    }
+})
+
+const { contextBridge } = require('electron')
+
+contextBridge.exposeInMainWorld('versions', {
+    node: () => process.versions.node,
+    chrome: () => process.versions.chrome,
+    electron: () => process.versions.electron
+})
