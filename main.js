09f0e9d735baae952e10d4eaecd93dfd4542da4c
yukino <3181137349go@gmail.com>
2023-06-15 08:49:48 +0800

    06154

--- main.js
+++ main.js
@@ -1,29 +1,53 @@
 const path = require('path')
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 const isDev = process.env.NODE_ENV !== 'development'
+const isMac = process.platform === 'darwin'
 
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'Yukino Image Resizer',
         width: isDev ? 1000 : 500,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        }
+        height: 600
     })
     mainWindow.loadFile(path.join(__dirname, 'renderer/index.html'))
-    // open devtools
     if (isDev) mainWindow.webContents.openDevTools()
+}
+
+function creatAboutWindow() {
+    const aboutWindow = new BrowserWindow({
+        title: 'About Yukino Image Resizer',
+        width: 300,
+        height: 300
+    })
+    aboutWindow.loadFile(path.join(__dirname, 'renderer/about.html'))
 }
 
 app.whenReady().then(() => {
     createMainWindow()
-
+    const mainMenu = Menu.buildFromTemplate(menu)
+    Menu.setApplicationMenu(mainMenu)
     app.on('activate', () => {
         if (!BrowserWindow.getAllWindows().length === 0) createMainWindow()
     })
 })
 
+const menu = [
+    ...(isMac ? [{
+        label: app.name,
+        submenu: [{
+            label: 'About',
+            click: creatAboutWindow
+        }]
+    }] : [{
+        label: 'Help',
+        submenu: [{
+            lable: 'About',
+            click: creatAboutWindow
+        }]
+    }]),
+    { role: 'fileMenu' },
+]
+
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+    if (!isMac) app.quit()
 })
